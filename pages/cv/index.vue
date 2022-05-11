b18bdec721485d1b0b11c43de8db97e9fbd1c662
Glitch (glitch-hello-node) <none>
2022-05-11 06:12:32 +0000

    🌆🎟 Checkpoint ./pages/cv/index.vue:289856/1593 ./public/style.css:289856/46

--- pages/cv/index.vue
+++ pages/cv/index.vue
@@ -1,18 +1,18 @@
 <template>
-  <dialog class="zs-modal" open>
+  <dialog class="generic-modal" open>
     <form method="dialog">
-      <div class="zs-modal--header">
-        <h2>Greetings, one and all!</h2>
+      <div class="generic-modal--header">
+        <h3>Resume</h3>
         <button class="btn btn-default-borderless btn-square btn-rounded">&times;</button>
       </div>
-      <div class="zs-modal--body">
-        <label help-text="You should probably write something useless here">Input Label
+      <div class="generic-modal--body">
+        <p>The contents of this CV contain sensitive information, so the contents are stowed under lock & key. If I’ve given you the password to access this, enter it below.</p>
+        <label>
           <input type="text" placeholder="put text in me daddy">
         </label>
       </div>
-      <div class="zs-modal--footer">
-        <button class="btn btn-default">Cancel</button>
-        <button class="btn btn-primary">Alright</button>
+      <div class="generic-modal--footer">
+        <button class="btn btn-primary">Submit</button>
       </div>
     </form>
   </dialog>
@@ -28,54 +28,65 @@ export default {
 </script>
 
 <style lang="scss">
-  .zs-modal {
-  display: flex;
-  border: 1px solid #eee;
-  border-radius: 8px;
-  background-color: var(--background);
-  padding: 0;
-  box-shadow: 0 6px 12px -3px rgba(0,0,0,0.2);
-  &::backdrop {
-    background: rgba(0,0,0,.7);
-  }
-  form {
-    display: grid;
-/*     grid-auto-rows: auto 1fr auto; */
-    grid-auto-rows: 1fr auto 1fr;
-    width: 100%;
-  }
-  
-  &--header,
-  &--footer {
+  .generic-modal {
+    position: absolute;
+    inset: 0;
     display: flex;
-    justify-content: space-between;
-    align-items: center;
-    gap: 1rem;
-    padding: 1rem;
-    user-select: none;
-  }
-  
-  &--header {
-    font-size: 18px;
-    border-bottom:  1px solid #eee;
-    * {
-      line-height: 1;
-      font-size: inherit !important;
-      margin: 0;
-      padding: 0 !important;
+    border: none;
+    padding: 0;
+    &:before {
+      pointer-events: none;
+      content: '';
+      position: fixed;
+      inset: 0;
+      background: var(--background);
+      opacity: .7;
+      z-index: 0;
+    }
+    form {
+      position: relative;
+      display: grid;
+  /*     grid-auto-rows: auto 1fr auto; */
+      grid-auto-rows: 1fr auto 1fr;
+      width: 100%;
+      max-width: 600px;
+      border: 1px solid #eee;
+      border-radius: 8px;
+      background-color: var(--background);
+      box-shadow: 0 6px 12px -3px rgba(0,0,0,0.2);
+      z-index: 1;
+    }
+
+    &--header,
+    &--footer {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      gap: 1rem;
+      padding: 1rem;
+      user-select: none;
+    }
+
+    &--header {
+      font-size: 18px;
+      border-bottom:  1px solid #eee;
+      * {
+        line-height: 1;
+        margin: 0;
+        padding: 0 !important;
+      }
+    }
+    &--body {
+      position: relative;
+      padding: 1rem;
+      &.handle-overflow {
+        max-height: 65vh;
+        overflow-y: auto;
+      }
+    }
+    &--footer {
+      justify-content: flex-end;
+      border-top:  1px solid #eee;
     }
   }
-  &--body {
-    position: relative;
-    padding: 1rem;
-    &.handle-overflow {
-      max-height: 65vh;
-      overflow-y: auto;
-    }
-  }
-  &--footer {
-    justify-content: flex-end;
-    border-top:  1px solid #eee;
-  }
-}
 </style>
