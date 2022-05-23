b05082158827fdb741dfa52cdc924b84d73d0c21
Glitch (glitch-hello-node) <none>
2022-05-23 15:00:23 +0000

    🐕✊ Checkpoint ./nuxt.config.js:289856/193 ./pages/contact/index.vue:289856/193

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,5 +7,10 @@ export default {
     cvpw: process.env.cvpw,
     API_URL: process.env.API_URL,
     CONTENT_KEY: process.env.CONTENT_KEY
+  },
+  pwa: {
+    icon: {
+      source: 'https://cdn.glitch.global/c2f99bc4-f921-4cee-90b5-df019f304eee/favicon.png?v=1652244090700'
+    }
   }
 }
--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -2,11 +2,11 @@
   <section class="container contact-container">
     <h2>Let’s Work Together</h2>
     <p>Looks like you saw something you liked 😉 Let’s chat! Feel free to utilize the form below, though if you prefefer email instead, you can to so through <a href="mailto:javidwaldron@gmail.com">javidwaldron@gmail.com</a></p>
-    <form method="post">
-      <input type="text" placeholder="Name" v-model="recipientName" />
-      <input type="text" placeholder="Email" v-model="recipientEmail" />
-      <textarea placeholder="Whats up" rows="6" v-model="recipientMessage"></textarea>
-      <button class="btn btn-primary">Send <icons name="arrow-right" /></button>
+    <form action="mailto:javidwaldron@gmail.com" method="GET" enctype="text/plain">
+      <input name="subject" type="text" placeholder="Name" v-model="recipientName" />
+      <input name="" type="text" placeholder="Email" v-model="recipientEmail" />
+      <textarea name="body" placeholder="Whats up" rows="6" v-model="recipientMessage"></textarea>
+      <button type="submit" value="send" class="btn btn-primary">Send <icons name="arrow-right" /></button>
     </form>
   </section>
 </template>
