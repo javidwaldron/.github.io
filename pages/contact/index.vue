432a7f5268658f76c746afa88a9c23085fe8c1f6
Glitch (glitch-hello-node) <none>
2022-05-23 15:12:31 +0000

    🙇🛢 Checkpoint ./pages/contact/index.vue:289856/2199

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="container contact-container">
     <h2>Let’s Work Together</h2>
-    <p>Looks like you saw something you liked 😉 Let’s chat! Feel free to utilize the form below, though if you prefefer email instead, you can to so through <a href="mailto:javidwaldron@gmail.com">javidwaldron@gmail.com</a></p>
-    <form action="mailto:javidwaldron@gmail.com" method="GET" enctype="text/plain">
+    <p v-if="!formSubmitted">Looks like you saw something you liked 😏 Let’s chat! Feel free to utilize the form below, though if you prefefer email instead, you can to so through <a href="mailto:javidwaldron@gmail.com">javidwaldron@gmail.com</a></p>
+    <form action="mailto:javidwaldron@gmail.com" method="GET" enctype="text/plain" @submit.prevent="submit()" v-if="!formSubmitted">
       <input name="subject" type="text" placeholder="Name" v-model="recipientName" />
       <input name="" type="text" placeholder="Email" v-model="recipientEmail" />
       <textarea name="body" placeholder="Whats up" rows="6" v-model="recipientMessage"></textarea>
@@ -22,7 +22,22 @@
       return {
         recipientName: '',
         recipientEmail: '',
-        recipientMessage: ''
+        recipientMessage: '',
+        formSubmitted: false
+      }
+    },
+    methods: {
+      submit() {
+        const reachingOut = {
+          id: Math.floor(Math.random() * 100000),
+          name : this.recipientName,
+          email : this.recipientEmail,
+          message : this.recipientMessage
+        }
+        this.$emit('client-data', reachingOut)
+        this.recipientName = ''
+        this.recipientEmail = ''
+        this.recipientMessage = ''
       }
     },
     components: {
