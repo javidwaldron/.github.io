224d9d72382d8dc2b742b1aff990d1ef1cd92542
ltrademark <ltrademark@users.noreply.github.com>
2022-07-20 15:56:32 +0000

    deploy: d820e0e93540fe204537fbd20c71c22e141fac69

---
+++ _nuxt/static/1658332591/contact/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fjavidwaldron.github.io\u002F_nuxt\u002Fstatic\u002F1658332591",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcontact",config:{_app:{basePath:"\u002Fjavidwaldron.github.io\u002F",assetsPath:"\u002Fjavidwaldron.github.io\u002F_nuxt\u002F",cdnURL:a}}}}(null));
