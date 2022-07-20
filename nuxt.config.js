ad21cf6af0a043e6ad9eefdaeaadcd6f65f2914c
Leo H <leo@ltrdmrk.co>
2022-07-20 11:56:39 -0400

    site not loading, maybe the base is messing things up

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,8 +28,5 @@ export default {
       source: 'https://cdn.glitch.global/c2f99bc4-f921-4cee-90b5-df019f304eee/favicon.png?v=1652244090700'
     }
   },
-  target: 'static',
-  router: {
-    base: '/javidwaldron.github.io/'
-  }
+  target: 'static'
 }
