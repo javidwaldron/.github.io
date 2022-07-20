c631bd22077079bf1aa8dc0336f8189cf86f3b18
Leo H <leo@ltrdmrk.co>
2022-07-20 12:06:31 -0400

    ok, completely reverting

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,6 +28,7 @@ export default {
       source: 'https://cdn.glitch.global/c2f99bc4-f921-4cee-90b5-df019f304eee/favicon.png?v=1652244090700'
     }
   },
+  ssr: false,
   target: 'static',
   router: {
     base: '/javidwaldron.github.io/'
